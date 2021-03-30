a1ff6e34f492f04a469f62ad345bdc3734086e04
Muhamad Reza Pahlevi <47028219+elpahlevi@users.noreply.github.com>
2021-03-30 22:18:00 +0700

    Create button component (atomic)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import Button from "./Button";
+
 const Header = ({ showAddToggle, showAdd, clearTodos }) => {
   return (
     <section className="header">
-      <button className="header-btn main-black-color" onClick={showAddToggle}>
-        {/* Pake Ternary */}
-        {showAdd ? "Finish" : "Add"}
-      </button>
+      <Button text={showAdd ? "Finish" : "Add"} onClick={showAddToggle} />
       <h1 className="header-title">Todo Lists</h1>
-      <button className="header-btn main-red-color" onClick={clearTodos}>
-        Clear
-      </button>
+      <Button text="Clear" onClick={clearTodos} color="red" align="right" />
     </section>
   );
 };
 
 Header.propTypes = {
   showAddToggle: PropTypes.func.isRequired,
-  showAdd: PropTypes.bool.isRequired
+  showAdd: PropTypes.bool.isRequired,
+  clearTodos: PropTypes.func.isRequired
 };
 
 export default Header;
