46ab705901c002f6278d7699acb66451ba64eeb1
Muhamad Reza Pahlevi <47028219+elpahlevi@users.noreply.github.com>
2021-04-05 18:39:36 +0700

    Emotion css props in button component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+/** @jsxImportSource @emotion/react */
+// import React from "react";
+// import { jsx } from "@emotion/react"; (automatically assigned)
 import PropTypes from "prop-types";
-import cx from "classnames";
+// import cx from "classnames";
 
-import styles from "./button.module.css";
+// import styles from "./button.module.css";
+import * as styles from "./button.styles";
 
 // && is a short circuit conditional [if true then do something]
 const Button = ({ text, onClick, color, align }) => {
@@ -14,15 +17,18 @@ const Button = ({ text, onClick, color, align }) => {
   //   align === "right" && "align-right"
   // ].join(" ");
 
-  const classNames = cx(styles.headerBtn, {
-    [styles.mainBlackColor]: color === "black",
-    [styles.mainRedColor]: color === "red",
-    [styles.alignLeft]: align === "left",
-    [styles.alignRight]: align === "right"
-  });
+  // const classNames = cx(styles.headerBtn, {
+  //   [styles.mainBlackColor]: color === "black",
+  //   [styles.mainRedColor]: color === "red",
+  //   [styles.alignLeft]: align === "left",
+  //   [styles.alignRight]: align === "right"
+  // });
 
   return (
-    <button className={classNames} onClick={onClick}>
+    // <button className={classNames} onClick={onClick}>
+    //   {text}
+    // </button>
+    <button css={styles.button({ align, color })} onClick={onClick}>
       {text}
     </button>
   );
