36c0e0c63e428c2aa00929f1e302923455885208
Muhamad Reza Pahlevi <47028219+elpahlevi@users.noreply.github.com>
2021-04-21 13:48:17 +0700

    Implement Layout in paper component

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -4,6 +4,7 @@ import Paper from "../components/paper/Paper";
 import Header from "../components/header/Header";
 import TodoForm from "../components/todoForm/TodoForm";
 import Todos from "../components/todos/Todos";
+import Container from "../layout/Container";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([
@@ -35,13 +36,19 @@ const TodoList = () => {
 
   return (
     <Paper>
-      <Header
-        showAddToggle={showAddToggle}
-        showAdd={showAdd}
-        clearTodos={clearTodos}
-      />
-      <TodoForm addTodo={addTodo} showAdd={showAdd} />
-      <Todos todos={todos} completeTodo={completeTodo} />
+      <Container
+        flexDirection="column"
+        justifyContent="space-between"
+        height="100%"
+      >
+        <Header
+          showAddToggle={showAddToggle}
+          showAdd={showAdd}
+          clearTodos={clearTodos}
+        />
+        <TodoForm addTodo={addTodo} showAdd={showAdd} />
+        <Todos todos={todos} completeTodo={completeTodo} />
+      </Container>
     </Paper>
   );
 };
